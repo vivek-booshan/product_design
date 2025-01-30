70330d3baa2e38cb33cb388d6668265a5a50e84f
Vivek Booshan <vivek.booshan@gmail.com>
2025-01-29 19:15:02 -0500

    add zig files

--- led.zig
+++ led.zig
@@ -1,21 +1,24 @@
 const std = @import("std");
 const wpi = @cImport({
     @cInclude("wiringPi.h");
-    @cDefine("LEDPIN", "0");
 });
+
+const LEDPIN = @as(c_int, 0);
 const DELAY = @as(c_uint, 500);
 
-pub fn main() void {
-    if (wpi.wiringPiSetup() == -1) {
-        std.debug.print("Setup wiringPi failed !", .{});
-    }
-    wpi.pinMode(wpi.LEDPIN, wpi.OUTPUT);
+pub fn main() !void {
+    try wpi.wiringPiSetup();
+    // if (wpi.wiringPiSetup() == -1) {
+    //     std.debug.print("Setup wiringPi failed !", .{});
+    // }
+    try wpi.pinMode(LEDPIN, wpi.OUTPUT);
     while (true) {
-        wpi.digitalWrite(wpi.LEDPIN, wpi.LOW);
+        try wpi.digitalWrite(LEDPIN, wpi.LOW);
         std.debug.print("...LED on\n", .{});
-        wpi.delay(DELAY);
-        wpi.digitalWrite(wpi.LEDPIN, wpi.HIGH);
+        try wpi.delay(DELAY);
+
+        try wpi.digitalWrite(LEDPIN, wpi.HIGH);
         std.debug.print("LED off...\n", .{});
-        wpi.delay(DELAY);
+        try wpi.delay(DELAY);
     }
 }
