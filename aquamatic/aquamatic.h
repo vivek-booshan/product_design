9127365b9b545a8326bfe378326618c8221fbbba
Vivek Booshan <vivek@Viveks-MacBook-Pro.local>
2025-02-10 16:01:06 -0500

    add timestamp and signal handler to header

--- aquamatic/aquamatic.h
+++ aquamatic/aquamatic.h
@@ -9,11 +9,13 @@
 #define PID_FILE "/tmp/aquamatic/daemon.pid"
 #define STATE_FILE "/tmp/aquamatic/state.txt"
 
-extern int tui_flag;
+extern volatile int tui_flag;
 
 void read_pid_file(FILE *pid_file, pid_t *pid);
 void run_daemon();
 void quit_daemon();
 void run_tui();
+void tui_signal_handler(int signum);
+void get_timestamp(char *buffer, size_t buffer_size);
 
 #endif
