8a7a2eee0ed1e17e1ac41e01cda51f7a950bdf5d
Vivek Booshan <vivek.booshan@gmail.com>
2025-04-14 17:01:53 -0400

    convert to uart protocol for communication

--- aquamatic/tsensor.c
+++ aquamatic/tsensor.c
@@ -1,130 +1,71 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <ctype.h>
-#include <linux/i2c-dev.h>
-#include <sys/ioctl.h>
 #include <termios.h>
+#include <errno.h>
 
-#define I2C_ADDR 0x66
-#define MAX_LEN 20
-
-static int open_i2c(void)
-{
-        int i2c_fd = open("/dev/i2c-1", O_RDWR);
-        if (i2c_fd < 0) {
-                perror("I2C open error");
-                exit(1);
-        }
-
-        if (ioctl(i2c_fd, I2C_SLAVE, I2C_ADDR) < 0) {
-                perror("I2C ioctl error");
-                close(i2c_fd);
-                exit(1);
-        }
-
-        return i2c_fd;
-}
-
-static int open_serial(void)
-{
-        int serial_fd = open("/dev/serial0", O_RDWR | O_NOCTTY | O_SYNC);
-        if (serial_fd < 0) {
-                perror("Serial open error");
-                exit(1);
-        }
-
-        struct termios tty;
-        memset(&tty, 0, sizeof(tty));
-        if (tcgetattr(serial_fd, &tty) != 0) {
-                perror("tcgetattr error");
-                exit(1);
-        }
-
-        cfsetospeed(&tty, B9600);
-        cfsetispeed(&tty, B9600);
-        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;    
-        tty.c_iflag = 0;                
-        tty.c_oflag = 0;                
-        tty.c_lflag = 0;                
-        tty.c_cc[VMIN]  = 1;            
-        tty.c_cc[VTIME] = 1;            
-
-        if (tcsetattr(serial_fd, TCSANOW, &tty) != 0) {
-                perror("tcsetattr error");
-                exit(1);
-        }
-
-        return serial_fd;
-}
-
-static inline void to_lowercase(char *str)
-{
-        for (; *str; ++str) *str = tolower(*str);
-}
+#define SERIAL_PORT "/dev/serial0"
+#define BAUD_RATE B9600
 
 int main(void)
 {
-        int i2c_fd = open_i2c();
-        int serial_fd = open_serial();
-
-        char buffer[MAX_LEN];
-        char rtd_data[MAX_LEN];
-        int time_delay;
-        char code;
-
-        while (1) {
-                int bytes_read = read(serial_fd, buffer, MAX_LEN - 1);
-                if (bytes_read > 0) {
-                        buffer[bytes_read] = '\0';
-
-                        // Remove trailing CR or LF
-                        char *newline = strchr(buffer, '\r');
-                        if (newline) *newline = '\0';
-                        newline = strchr(buffer, '\n');
-                        if (newline) *newline = '\0';
-
-                        to_lowercase(buffer);
-
-                        time_delay = (buffer[0] == 'c' || buffer[0] == 'r') ? 600 : 250;
-
-                        // Write to I2C
-                        if (write(i2c_fd, buffer, strlen(buffer)) != strlen(buffer)) {
-                                perror("I2C write failed");
-                        }
-
-                        if (strcmp(buffer, "sleep") != 0) {
-                                usleep(time_delay * 1000);
-
-                                // Request 20 bytes from I2C
-                                if (read(i2c_fd, &code, 1) != 1) {
-                                    perror("I2C read failed (code)");
-                                    continue;
-                                }
-
-                                switch (code) {
-                                case 1: printf("Success\n"); break;
-                                case 2: printf("Failed\n"); break;
-                                case 254: printf("Pending\n"); break;
-                                case 255: printf("No Data\n"); break;
-                                default: printf("Unknown code: %d\n", code);
-                                }
-
-                                // Read rest of data
-                                int r = read(i2c_fd, rtd_data, MAX_LEN);
-                                if (r > 0) {
-                                    rtd_data[r] = '\0';
-                                    printf("%s\n", rtd_data);
-                                }
-                        }
-                }
-
-        usleep(100000);  // 100ms loop delay
+        int serial_port = open(SERIAL_PORT, O_RDWR | O_NOCTTY | O_NDELAY);
+        if (serial_port < 0) {
+                perror("Error opening serial port");
+                return 1;
         }
 
-    close(i2c_fd);
-    close(serial_fd);
-    return 0;
+        struct termios tty;
+        if (tcgetattr(serial_port, &tty) != 0) {
+                perror("Error getting terminal attributes");
+                close(serial_port);
+                return 1;
+        }
+
+        cfsetispeed(&tty, BAUD_RATE);
+        cfsetospeed(&tty, BAUD_RATE);
+
+        tty.c_cflag &= ~PARENB; // no parity
+        tty.c_cflag &= ~CSTOPB; // 1 stop bit
+        tty.c_cflag &= ~CSIZE;
+        tty.c_cflag |= CS8;
+
+        tty.c_cflag &= ~CRTSCTS; // no flow ctrl
+        tty.c_cflag |= CREAD | CLOCAL;
+
+        tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // raw input
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // no software flow ctrl
+        tty.c_oflag &= ~OPOST; // raw output
+
+        /* set timeout (non block read) */
+        tty.c_cc[VMIN] = 0;
+        tty.c_cc[VTIME] = 10; // timeout in 100ms incr
+
+        if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+                perror("Error setting terminal attributes");
+                close(serial_port);
+                return 1;
+        }
+
+        printf("Listening on %s...\n", SERIAL_PORT);
+
+        char read_buf[512];
+        memset(&read_buf, '\0', sizeof(read_buf));
+
+        while (1) {
+                int num_bytes = read(serial_port, &read_buf, sizeof(read_buf));
+                if (num_bytes < 0) {
+                        if (errno == EAGAIN) continue;
+                        perror("Error reading from serial port");
+                        break;
+                } else if (num_bytes > 0) {
+                        read_buf[num_bytes] = '\0';
+                        printf("%s", read_buf);
+
+                }
+                usleep(100000); // 100ms
+        }
+        close(serial_port);
+        return 0;
 }
